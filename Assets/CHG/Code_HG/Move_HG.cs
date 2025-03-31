96691a0f319787f78d077da9de57a4d6ce463886
2021CGR <abby12166219@g.shingu.ac.kr>
2025-03-31 17:58:13 +0900

    [Feat]캐릭터 이동코드와 E로 상호작용을 했을 때 타임라인 실행
    캐릭터 이동코드와 E로 상호작용을 했을 때 상호작용한 오브젝트가 사라지고 타임라인이 실행되고 2번째 타임라인이 끝났을 때 다음 씬으로 넘어갈 수 있게 하는 코드

---
+++ Assets/CHG/Code_HG/Move_HG.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 1.5f;
+
+    private CharacterController controller;
+    private Vector3 velocity;
+    private bool isGrounded;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        // 바닥 체크
+        isGrounded = controller.isGrounded;
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        // 키 입력 받기
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+
+        // 이동 적용
+        controller.Move(move * speed * Time.deltaTime);
+
+        // 점프
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        // 중력 적용
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
