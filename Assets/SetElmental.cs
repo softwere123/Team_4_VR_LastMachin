f9f8dcc8de987b543b46e0b75e50ab19332c9c9e
2021CGR <baby12166219@g.shingu.ac.kr>
2025-04-08 01:09:16 +0900

    [Feat]UTL

---
+++ Assets/SetElmental.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Autohand;
+
+public class SetElemental : MonoBehaviour
+{
+    public List<DistanceGrabbable> magneticObjects; // 마그네틱: DistanceGrabbable 리스트
+    public List<Grabbable> fireObjects;             // 파이어: Grabbable 리스트
+
+    public void SetType(int index)
+    {
+        Debug.Log($"SetType 호출됨, index: {index}");
+
+        bool isMagnetic = (index == 1);
+        bool isFire = (index == 2);
+
+        SetMagneticObjects(isMagnetic);
+        SetGrabbable(fireObjects, isFire);
+    }
+
+    private void SetMagneticObjects(bool enable)
+    {
+        foreach (var distanceGrab in magneticObjects)
+        {
+            distanceGrab.enabled = enable;
+            Debug.Log($"Magnetic Object '{distanceGrab.gameObject.name}' set to {(enable ? "ENABLED" : "DISABLED")}");
+        }
+    }
+
+    private void SetGrabbable(List<Grabbable> grabbables, bool enable)
+    {
+        foreach (var grab in grabbables)
+        {
+            grab.enabled = enable;
+            Debug.Log($"Fire Object '{grab.gameObject.name}' set to {(enable ? "ENABLED" : "DISABLED")}");
+        }
+    }
+}
