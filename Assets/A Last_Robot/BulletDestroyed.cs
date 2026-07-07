bce4d87dcddd976d510152ccb87f563828c8ff17
Myungseo++ <aa6053bb@gmail.com>
2026-07-07 18:20:29 +0900

    feat bullet funtion

---
+++ Assets/A Last_Robot/BulletDestroyed.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDestroyed : MonoBehaviour
+{
+
+    private void Start()
+    {
+        PlayerHP = PlayerMax;
+    }
+
+    public int PlayerHP;
+    public int PlayerMax = 100;
+
+    public void PlayerDamaged(int Damage)
+    {
+        PlayerHP -= Damage;
+
+        if (PlayerHP < 0)
+        {
+            Debug.Log("실행2");
+
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        this.gameObject.SetActive(false);
+        GameManager.instance.Restart.SetActive(true);
+    }
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+
+}
---
+++ Assets/A Last_Robot/GameManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+// restart 기능을 만들자
+// 재시작 할 때 image restart를 보이지 않게 하자
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance { get; private set; }
+
+    public void OnClickRestart()
+    {
+        //첫 장면을 가져오게 된다.
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public GameObject Restart;
+    public GameObject Restart2;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+}
